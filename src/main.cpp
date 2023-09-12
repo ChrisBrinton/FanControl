f0cfe14ce414c29a1e7e6972b9c7bead35ab6be9
Chris Brinton <cbrinton@gmail.com>
2023-09-12 14:06:28 -0400

    example buttons

--- src/main.cpp
+++ src/main.cpp
@@ -47,22 +47,35 @@ void lv_ex_btn_1(void)
 {
     lv_obj_t * label;
 
-    lv_obj_t * btn1 = lv_btn_create(lv_scr_act(), NULL);
-    lv_obj_set_event_cb(btn1, event_handler);
-    lv_obj_align(btn1, NULL, LV_ALIGN_CENTER, 0, -40);
+    lv_obj_t * btn_preset_1 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn_preset_1, event_handler);
+    lv_obj_align(btn_preset_1, NULL, LV_ALIGN_CENTER, 140, -40);
+    lv_btn_set_checkable(btn_preset_1, true);
+    lv_btn_toggle(btn_preset_1);
+    lv_btn_set_fit2(btn_preset_1, LV_FIT_NONE, LV_FIT_TIGHT);
 
-    label = lv_label_create(btn1, NULL);
-    lv_label_set_text(label, "Button");
+    label = lv_label_create(btn_preset_1, NULL);
+    lv_label_set_text(label, "PLA");
 
-    lv_obj_t * btn2 = lv_btn_create(lv_scr_act(), NULL);
-    lv_obj_set_event_cb(btn2, event_handler);
-    lv_obj_align(btn2, NULL, LV_ALIGN_CENTER, 0, 40);
-    lv_btn_set_checkable(btn2, true);
-    lv_btn_toggle(btn2);
-    lv_btn_set_fit2(btn2, LV_FIT_NONE, LV_FIT_TIGHT);
+    lv_obj_t * btn_preset_2 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn_preset_2, event_handler);
+    lv_obj_align(btn_preset_2, NULL, LV_ALIGN_CENTER, 140, 30);
+    lv_btn_set_checkable(btn_preset_2, true);
+    //lv_btn_toggle(btn_preset_2);
+    lv_btn_set_fit2(btn_preset_2, LV_FIT_NONE, LV_FIT_TIGHT);
 
-    label = lv_label_create(btn2, NULL);
-    lv_label_set_text(label, "Toggled");
+    label = lv_label_create(btn_preset_2, NULL);
+    lv_label_set_text(label, "PETG");
+
+    lv_obj_t * btn_preset_3 = lv_btn_create(lv_scr_act(), NULL);
+    lv_obj_set_event_cb(btn_preset_3, event_handler);
+    lv_obj_align(btn_preset_3, NULL, LV_ALIGN_CENTER, 140, 100);
+    lv_btn_set_checkable(btn_preset_3, true);
+    //lv_btn_toggle(btn_preset_3);
+    lv_btn_set_fit2(btn_preset_3, LV_FIT_NONE, LV_FIT_TIGHT);
+
+    label = lv_label_create(btn_preset_3, NULL);
+    lv_label_set_text(label, "ABS");
 }
 
 /**********************
